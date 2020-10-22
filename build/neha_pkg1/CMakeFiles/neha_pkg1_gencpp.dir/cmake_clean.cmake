0d783ad7f83f8592cd10fd20aae2ec3f5ac9b473
nehamarne27 <nehamarne27@gmail.com>
2020-10-22 17:04:36 +0530

    build deleted

--- build/neha_pkg1/CMakeFiles/neha_pkg1_gencpp.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/neha_pkg1_gencpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/neha_pkg1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neha/neha1/src/neha_pkg1
-# Build directory: /home/neha/neha1/build/neha_pkg1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
