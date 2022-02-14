6a42b295dfa6f19be9720059e29aa31c259b222c
nehamarne27 <51331480+nehamarne27@users.noreply.github.com>
2022-02-14 22:06:02 +0530

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/neha/neha1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/neha/neha1/devel;/home/neha/workspace/devel;/home/neha/mrs_workspace/devel;/opt/ros/melodic'.split(';') if '/home/neha/neha1/devel;/home/neha/workspace/devel;/home/neha/mrs_workspace/devel;/opt/ros/melodic' != '' else []
